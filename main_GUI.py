8803429851a5d3d5b184754e4cb3ab45b4e2427c
Milan Grujicic <milangru@gmail.com>
2022-11-14 19:59:19 +0100

    Adding window geometry centralization and catch exception for strings and symbols

--- main_GUI.py
+++ main_GUI.py
@@ -6,15 +6,32 @@ random_string = string.ascii_letters + string.digits + string.punctuation
 
 root = Tk()
 
-root.title('Password Generator')
+root.title(
+    'Password Generator - Generate password with lenght from 1 to 94 characters')
 
-root.geometry("450x300")
+window_width = 400
+window_height = 200
+
+# Get the screen dimension
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# Find the center point
+center_x = int(screen_width/2 - window_width / 10)
+center_y = int(screen_height/2 - window_height / 10)
+
+# Set the position of the window to the center of the screen
+root.geometry(f"{window_width}x{window_height}+{center_x}+{center_y}")
 
 
 def generatePasswordButtonClicked():
-    lenght = int(userInput.get())
-    password = ''.join(random.sample(random_string, lenght))
-    resultLabel.configure(text="Password: " + password)
+    try:
+        lenght = int(userInput.get())
+    except ValueError:
+        resultLabel.configure(text="Wrong input")
+    else:
+        password = ''.join(random.sample(random_string, lenght))
+        resultLabel.configure(text="Password: " + password)
 
 
 def cancelButtonClicked():
@@ -24,20 +41,20 @@ def cancelButtonClicked():
 generateButton = Button(root, text='Create Password',
                         command=generatePasswordButtonClicked)
 
-generateButton.grid(row=0, column=0)
+generateButton.pack()
 
 closeButton = Button(root, text='Close', command=cancelButtonClicked)
 
 closeButton.config(bg='#9FD996')
 
-closeButton.grid(row=1, column=0)
+closeButton.pack()
 
 userInput = Entry(root, width=10)
 
-userInput.grid(row=2, column=0)
+userInput.pack()
 
 resultLabel = Label(root, text="Password: ")
 
-resultLabel.grid(row=3, column=0)
+resultLabel.pack()
 
 root.mainloop()
