0d99e06404cfcd7c70f99c9a6a02875148ee12b8
Milan Grujicic <milangru@gmail.com>
2022-11-18 16:59:04 +0100

    Adding formatting

--- main_CLI.py
+++ main_CLI.py
@@ -7,8 +7,7 @@ random_string = string.ascii_letters + string.digits + string.punctuation
 # Variable type verification
 while True:
     try:
-        lenght = int(
-            input("Choose the lenght of the password (Only integer)\n> "))
+        lenght = int(input("Choose the lenght of the password (Only integer)\n> "))
     except ValueError:
         print("This is an unaccepted response, enter a valid value")
         continue
@@ -16,5 +15,5 @@ while True:
         break
 
 # Creates password based on user's input
-password = ''.join(random.sample(random_string, lenght))
+password = "".join(random.sample(random_string, lenght))
 print(password)
--- main_GUI.py
+++ main_GUI.py
@@ -6,8 +6,7 @@ random_string = string.ascii_letters + string.digits + string.punctuation
 
 root = Tk()
 
-root.title(
-    'Password Generator - Generate password with lenght from 1 to 94 characters')
+root.title("Password Generator - Generate password with lenght from 1 to 94 characters")
 
 window_width = 400
 window_height = 200
@@ -17,8 +16,8 @@ screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 
 # Find the center point
-center_x = int(screen_width/2 - window_width / 10)
-center_y = int(screen_height/2 - window_height / 10)
+center_x = int(screen_width / 2 - window_width / 10)
+center_y = int(screen_height / 2 - window_height / 10)
 
 # Set the position of the window to the center of the screen
 root.geometry(f"{window_width}x{window_height}+{center_x}+{center_y}")
@@ -32,16 +31,18 @@ def validationCheck():
             lenght = int(userInput.get())
         except ValueError:
             resultLabel.configure(
-                text="Input cannot be string nor symbol\nPlease insert value betwixt 1 and 95")
+                text="Input cannot be string nor symbol\nPlease insert value betwixt 1 and 95"
+            )
         else:
             generatePasswordButtonClicked(lenght)
     except ValueError:
         resultLabel.configure(
-            text="Input cannot be less than 0 nor greater than 95\nPlease insert value betwixt 1 and 95")
+            text="Input cannot be less than 0 nor greater than 95\nPlease insert value betwixt 1 and 95"
+        )
 
 
 def generatePasswordButtonClicked(lenght):
-    password = ''.join(random.sample(random_string, lenght))
+    password = "".join(random.sample(random_string, lenght))
     resultLabel.configure(text="Password: \n" + password)
 
 
@@ -53,16 +54,15 @@ def altKeyPressL_Pressed(event):
     root.destroy()
 
 
-generateButton = Button(root, text='Create Password',
-                        command=validationCheck)
+generateButton = Button(root, text="Create Password", command=validationCheck)
 
 generateButton.pack()
 
-closeButton = Button(root, text='Cl\u0332ose', command=cancelButtonClicked)
+closeButton = Button(root, text="Cl\u0332ose", command=cancelButtonClicked)
 
-closeButton.bind('<Alt-KeyPress-l>', altKeyPressL_Pressed)
+closeButton.bind("<Alt-KeyPress-l>", altKeyPressL_Pressed)
 
-closeButton.config(bg='#9FD996')
+closeButton.config(bg="#9FD996")
 
 closeButton.focus()
 
