c325b37f6f230901afbcdfb9b58cc17752017c58
Milan <milangru@gmail.com>
2024-04-10 13:28:28 +0200

    Minor syntax fixes

--- main_CLI.py
+++ main_CLI.py
@@ -8,7 +8,6 @@ MAX_LENGTH = 30
 MIN_LENGTH = 8
 remaining = MAX_LENGTH - 2  # at least 1 lettter, number and special character
 
-
 def generate_password(n_letters, n_numbers, n_symbols):
     """Generate random password with number of letters, numbers and symbols defined by user"""
     letters = "".join(random.sample(string.ascii_letters, n_letters))
@@ -18,33 +17,33 @@ def generate_password(n_letters, n_numbers, n_symbols):
     return "".join(random.sample(password, len(password)))
 
 
-def get_number(prompt, range=None):
+def get_input(prompt, range=None):
     """Force user to input number. If range provided, input must be in range."""
     while True:
         try:
             value = int(input(prompt))
             if not range or range[0] <= value <= range[1]:
                 return value
-            print("[!]Invalid input.  Try again")
+            print("[!] Invalid input.  Try again")
         except ValueError:
-            print("[!]Invalid input.  Try again")
+            print("[!] Invalid input.  Try again")
 
 
 # User inputs amount of special characters, numbers and letters
-special = get_number(
+special = get_input(
     f"[-] How many special characters (1-{remaining}): ", (1, remaining)
 )
 remaining = remaining - special + 1
 
 if remaining > 2:
-    numbers = get_number(f"[-] How many numbers (1-{remaining}): ", (1, remaining))
+    numbers = get_input(f"[-] How many numbers (1-{remaining}): ", (1, remaining))
 else:
     numbers = 1
 
 remaining = remaining - numbers + 1
 if remaining > 1:
     min_letters = max(1, MIN_LENGTH - (special + numbers))
-    letters = get_number(
+    letters = get_input(
         f"[-] How many letters ({min_letters}-{remaining}): ", (min_letters, remaining)
     )
 else:
