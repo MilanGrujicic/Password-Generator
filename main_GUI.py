965cb03b1aa70ebb792f2208ab66e50279654ee8
Milan <milangru@gmail.com>
2024-04-10 21:26:48 +0200

    Moved some functions around.

--- main_GUI.py
+++ main_GUI.py
@@ -2,28 +2,6 @@ from tkinter import *
 import random
 import string
 
-random_string = string.ascii_letters + string.digits + string.punctuation
-
-root = Tk()
-
-root.title("Password Generator - Generate password with lenght from 1 to 94 characters")
-
-window_width = 400
-window_height = 200
-
-# Get the screen dimension
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-
-# Find the center point
-center_x = int(screen_width / 2 - window_width / 10)
-center_y = int(screen_height / 2 - window_height / 10)
-
-# Set the position of the window to the center of the screen
-root.geometry(f"{window_width}x{window_height}+{center_x}+{center_y}")
-
-root.resizable(False, False)
-
 def validationCheck():
     try:
         try:
@@ -49,6 +27,28 @@ def cancelButtonClicked():
 def altKeyPressL_Pressed(event):
     root.destroy()
 
+
+random_string = string.ascii_letters + string.digits + string.punctuation
+
+root = Tk()
+
+root.title("Password Generator - Generate password with lenght from 1 to 94 characters")
+
+window_width = 400
+window_height = 200
+
+# Get the screen dimension
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# Find the center point
+center_x = int(screen_width / 2 - window_width / 10)
+center_y = int(screen_height / 2 - window_height / 10)
+
+# Set the position of the window to the center of the screen
+root.geometry(f"{window_width}x{window_height}+{center_x}+{center_y}")
+
+root.resizable(False, False)
 
 generateButton = Button(root, text="Create Password", command=validationCheck)
 
