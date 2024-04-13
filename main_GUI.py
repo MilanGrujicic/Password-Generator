8e81857e79119fa780177830bc3522b1ba657cf4
Milan <milangru@gmail.com>
2024-04-14 01:40:38 +0200

    Added new GUI design and logo.

--- main_GUI.py
+++ main_GUI.py
@@ -1,75 +1,33 @@
+from logo import logo
+from utils import *
 from tkinter import *
-import random
-import string
 
-def validationCheck():
-    try:
-        try:
-            lenght = int(userInput.get())
-        except ValueError:
-            resultLabel.configure(
-                text="Input cannot be string nor symbol\nPlease insert value betwixt 1 and 95"
-            )
-        else:
-            generatePasswordButtonClicked(lenght)
-    except ValueError:
-        resultLabel.configure(
-            text="Input cannot be less than 0 nor greater than 95\nPlease insert value betwixt 1 and 95"
-        )
+class input():
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
 
-def generatePasswordButtonClicked(lenght):
-    password = "".join(random.sample(random_string, lenght))
-    resultLabel.configure(text="Password: \n" + password)
+class GUI:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("GUI")
+        self.window.config(bg="#004AAD")
+        self.window.geometry("500x500")
 
-def cancelButtonClicked():
-    root.destroy()
+        # LOGO
+        self.logo = PhotoImage(file="logo.png")  # Load the image
 
-def altKeyPressL_Pressed(event):
-    root.destroy()
+        # Create a canvas with the size of the logo or larger
+        canvas_width = self.logo.width()
+        canvas_height = self.logo.height()
+        canvas = Canvas(self.window, width=canvas_width, height=canvas_height, bg="#004AAD", highlightthickness=0)
+        canvas.grid(row=0, column=0)
+
+        # Centering the image in the canvas
+        canvas.create_image(canvas_width // 2, canvas_height // 2, image=self.logo)
+        
+        self.window.mainloop()
 
 
-random_string = string.ascii_letters + string.digits + string.punctuation
-
-root = Tk()
-
-root.title("Password Generator - Generate password with lenght from 1 to 94 characters")
-
-window_width = 400
-window_height = 200
-
-# Get the screen dimension
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-
-# Find the center point
-center_x = int(screen_width / 2 - window_width / 10)
-center_y = int(screen_height / 2 - window_height / 10)
-
-# Set the position of the window to the center of the screen
-root.geometry(f"{window_width}x{window_height}+{center_x}+{center_y}")
-
-root.resizable(False, False)
-
-generateButton = Button(root, text="Create Password", command=validationCheck)
-
-generateButton.pack()
-
-closeButton = Button(root, text="Cl\u0332ose", command=cancelButtonClicked)
-
-closeButton.bind("<Alt-KeyPress-l>", altKeyPressL_Pressed)
-
-closeButton.config(bg="#9FD996")
-
-closeButton.focus()
-
-closeButton.pack()
-
-userInput = Entry(root, width=10)
-
-userInput.pack()
-
-resultLabel = Label(root, text="Password: ")
-
-resultLabel.pack()
-
-root.mainloop()
+if __name__ == "__main__":
+    app = GUI()
