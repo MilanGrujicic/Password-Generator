6af30d7b2b607e38bc50a16fe7e5692eb1aacfa4
Milan <milangru@gmail.com>
2024-04-17 17:04:42 +0200

    Added scales to GUI.

--- main_GUI.py
+++ main_GUI.py
@@ -14,22 +14,34 @@ class GUI:
 
         # Create a canvas with the size of the logo or larger
         canvas_width = self.logo.width()
-        canvas_height = self.logo.height()
-        canvas = Canvas(self.window, width=canvas_width, height=canvas_height, bg="#004AAD", highlightthickness=0)
+        canvas_height = self.logo.height() // 2
+        canvas = Canvas(self.window, width=canvas_width, height=canvas_height, bg="#FF3131", highlightthickness=0)
         canvas.grid(row=0, column=0)
 
         # Centering the image in the canvas
         canvas.create_image(canvas_width // 2, canvas_height // 2, image=self.logo)
-
-
-        # SCALE 
-        self.Scala = Scale(self.window, from_=0, to=10, orient=HORIZONTAL, bg="#fff500", highlightbackground = "black", border="2")
-        self.Scala.grid(row=1, column=0)
         
+        self.numbers = Label(self.window, text="Letters", bg="#FF3131", fg="white")
+        self.numbers.grid(row=1, column=0)
+
+        self.scale_numbers = Scale(self.window, from_=0, to=10, orient=HORIZONTAL, bg="#fff500", highlightbackground = "black", border="2")
+        self.scale_numbers.grid(row=2, column=0, pady=5)
+
+        self.letters = Label(self.window, text="Numbers", bg="#FF3131", fg="white")
+        self.letters.grid(row=3, column=0)
+
+        self.scale_letters = Scale(self.window, from_=0, to=10, orient=HORIZONTAL, bg="#fff500", highlightbackground = "black", border="2")
+        self.scale_letters.grid(row=4, column=0, pady=5)
+
+        self.special_characters = Label(self.window, text="Special Characters", bg="#FF3131", fg="white")
+        self.special_characters.grid(row=5, column=0)
+
+        self.scale_characters = Scale(self.window, from_=0, to=10, orient=HORIZONTAL, bg="#fff500", highlightbackground = "black", border="2")
+        self.scale_characters.grid(row=6, column=0, pady=5)
 
         # BUTTONS
         self.close_button = Button(text="Exit", width=10, bg="#fff500", highlightbackground = "black", border="2", command=self.window.destroy)
-        self.close_button.grid(row=2, column=0)
+        self.close_button.grid(row=7, column=0)
 
         self.window.mainloop()
 
